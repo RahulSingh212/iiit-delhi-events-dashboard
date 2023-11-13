0a2bcfb8594490b49fff08f6c528d695c39fe562
RahulSingh212 <rahulsinghrs174326@gmail.com>
2023-11-13 16:31:14 +0530

    updated-next-config-file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  plugins: [require("tailwindcss")],
   css: ["styles/global.css"],
   images: {
     remotePatterns: [
